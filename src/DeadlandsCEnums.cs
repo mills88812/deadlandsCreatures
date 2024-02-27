ccc657cb07884167c95d61d52f79ec8b20e0c758
croken85 <161467495+croken85@users.noreply.github.com>
2024-02-27 18:35:17 +0100

    lizard port to fisobs

--- src/DeadlandsCEnums.cs
+++ src/DeadlandsCEnums.cs
@@ -23,18 +23,36 @@ namespace DeadlandsCreatures
     public class Type
     {
         public static CreatureTemplate.Type Buzzard;
+        public static CreatureTemplate.Type Iguana;
+        public static CreatureTemplate.Type BrownLizard;
+        public static CreatureTemplate.Type GlowLizard;
         public static CreatureTemplate.Type CandleMouse;
         public static CreatureTemplate.Type SpinePlant;
         public static CreatureTemplate.Type SaltWorm;
         public static void RegisterValues()
         {
             Buzzard = new CreatureTemplate.Type("Buzzard", true);
+            Iguana = new CreatureTemplate.Type("IguanaLizard", true);
+            BrownLizard = new CreatureTemplate.Type("BrownLizard", true);
+            GlowLizard = new CreatureTemplate.Type("GlowLizard", true);
         }
         public static void UnregisterValues()
         {
             if (Buzzard != null)
             {
                 Buzzard.Unregister();
+            }
+            if (Iguana != null)
+            {
+                Iguana.Unregister();
+            }
+            if (BrownLizard != null)
+            {
+                BrownLizard.Unregister();
+            }
+            if (GlowLizard != null)
+            {
+                GlowLizard.Unregister();
             }
         }
     }
